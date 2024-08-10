f9565c5c3b227af8277c370996e0c5731372a15f
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2024-08-10 19:55:50 +0530

    finalized multipage layout

---
+++ Home.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import st_pages
+
+# Set the title of the app
+st.title("Anomaly Detection Algo's Simulations")
+
+# Create a sidebar for navigation
+st.sidebar.title("Navigation")
---
+++ pages/1_Local Outlier Factor (LOF).py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("Local Outlier Factor (LOF) Simulation")
+st.sidebar.title('Inputs')
+st.sidebar.number_input('Inputs', 1, 10)
